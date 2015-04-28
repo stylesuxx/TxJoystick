9faa2d94301ebb306031f2b8c6e74249afc47f6a
stylesuxx <stylesuxx@gmail.com>
2015-04-28 20:27:46 +0200

    Moved code from ino to cpp and h. Added multiwii inspired text to ino file.

---
+++ TxJoystick.h
@@ -0,0 +1,14 @@
+#ifndef TXJOYSTICK_H_
+#define TXJOYSTICK_H_
+
+#define VERSION 140
+
+/**
+ * Input Modes
+ */
+#define ONOFF   0
+#define TRI     1
+#define SWITCH  2
+#define ANALOG -1
+
+#endif
--- TxJoystick.ino
+++ TxJoystick.ino
@@ -1,93 +1,12 @@
-#include <StandardCplusplus.h>
-#include <vector>
-#include <iterator>
-
-#include "avr/eeprom.h"
-#include "Config.h"
-#include "Channel.h"
-#include "PPM.h"
-
-/**
- * An Arduino based R/C Transmitter for use with a Joystick, Joypad or any other input device
+/*
+ * Welcome to TxJoystick.
  *
- * @author Chris Landa
+ * If you see this message, chances are you are using the Arduino IDE. That is ok.
+ * To get the TxJoystick program configured for your controller, you must switch to the tab named 'config.h'.
+ * Maybe that tab is not visible in the list at the top, then you must use the drop down list at the right
+ * to access that tab. In that tab you must enable the settings relevant to your controller.
+ *
+ * Have fun, and do not forget TxJoystick is made possible and brought to you under the GPL License.
+ *
+ * This public service anouncement has mostly been stolen from the MultiWii ino
  */
-
-std::vector<Channel> channels;
-PPM* ppm = NULL;
-
-/* Interrupt service routine which is responsible to write the PPM pulse */
-ISR(TIMER1_COMPA_vect) {
-  if(ppm) ppm->write();
-}
-
-void setup() {
-  /* Set Timer Counter Controll Register for Timer1 */
-  TCCR1A = B00110001; // Compare register B used in mode '3'
-  TCCR1B = B00010010; // WGM13 and CS11 set to 1
-  TCCR1C = B00000000; // All set to 0
-  TIMSK1 = B00000010; // Interrupt on compare B
-  OCR1A = 22500;      // 22,5mS PPM output refresh
-
-  if(DEBUG) {
-    Serial.begin(115200);
-    Serial.println("Debug Mode:");
-  }
-
-  /* Initialize EEPROM for the trims, this only needs to be done once */
-  if(INIT_EEPROM) {
-    signed int zero = 0;
-    eeprom_write_block(&zero, (void *)0, sizeof(signed int));
-    eeprom_write_block(&zero, (void *)16, sizeof(signed int));
-    eeprom_write_block(&zero, (void *)32, sizeof(signed int));
-  }
-
-  if(BUZZER) {
-    /* Beep three times to signalize that we are online */
-    for(int i = 0; i < 3; ++i) {
-      digitalWrite(BUZZER_PIN, HIGH);
-      delay(50);
-      digitalWrite(BUZZER_PIN, LOW);
-      delay(50);
-    }
-  }
-
-  /* Channels 1-4 in order they are listed */
-  Channel throttle(THROTTLE_PIN, true);
-  Channel yaw(YAW_PIN, false, YAW_UP_PIN, YAW_DOWN_PIN, 0);
-  Channel pitch(PITCH_PIN, true, PITCH_UP_PIN, PITCH_DOWN_PIN, 16);
-  Channel roll(ROLL_PIN, false, ROLL_UP_PIN, ROLL_DOWN_PIN, 32);
-
-  /**
-   * Feel free to add more AUX channels here but do not forget
-   * to push them on the vector in the setup method.
-   */
-  Channel aux1(AUX_1_PIN, false, SWITCH);
-  //Channel aux2(AUX_2_PIN, false, SWITCH);
-  //Channel aux3(AUX_3_PIN, ONOFF);
-  //Channel aux4(AUX_4_PIN, TRI);
-
-  throttle.adjust(THROTTLE_MIN, THROTTLE_MAX);
-  yaw.adjust(YAW_MIN, YAW_MAX);
-  pitch.adjust(PITCH_MIN, PITCH_MAX);
-  roll.adjust(ROLL_MIN, ROLL_MAX);
-
-  yaw.readCenter();
-  pitch.readCenter();
-  roll.readCenter();
-
-  channels.push_back(throttle);
-  channels.push_back(yaw);
-  channels.push_back(pitch);
-  channels.push_back(roll);
-  channels.push_back(aux1);
-  //channels.push_back(aux2);
-  /* Push additional channels here */
-  ppm = new PPM(PPM_PIN, &channels);
-}
-
-void loop() {
-  for (std::vector<Channel>::iterator ch = channels.begin() ; ch != channels.end(); ++ch) {
-    ch->read();
-  }
-}
